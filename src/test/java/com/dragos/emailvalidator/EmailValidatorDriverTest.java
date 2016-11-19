c8ea7563d2fbbdb17b28841da8454a1461a85ce8
Mustafa Assaf <massaf@cisco.com>
2016-11-19 20:08:01 +0200

    as

--- src/test/java/com/dragos/emailvalidator/EmailValidatorDriverTest.java
+++ src/test/java/com/dragos/emailvalidator/EmailValidatorDriverTest.java
@@ -33,6 +33,7 @@ public class EmailValidatorDriverTest  extends TestCase {
     }
     
     public void testIsEmailValid(){
-    	assertTrue( true );
+    	EmailValidatorDriver emailValidatorDriver = new EmailValidatorDriver("", "");
+    	 assertTrue( true );
     }
 }
